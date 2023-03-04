4bfbd4cbaeea7362038c9b8ef835d038b52ab44c
Serhii Shemendiuk <96024675+SerShem@users.noreply.github.com>
2023-03-04 15:29:25 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,30 +1,16 @@
 import { galleryItems } from './gallery-items.js';
-/* import { lightboxInport } from '/simplelightbox-master/dist/simple-lightbox.min'; */
 // Change code below this line
-
-console.log(galleryItems);
 
 const galleryConteiner = document.querySelector(`.gallery`);
 
 const galleryImages = galleryItems
 	.reduce((acum, galleryItems) =>
-		acum += `<a class="gallery__item" href="${galleryItems.original}.jpg">
-		<img class="gallery__image" src="${galleryItems.preview}" alt="${galleryItems.description}" />
-	</a>`, '')
+		acum += `<a class="gallery__item" href="${galleryItems.original}">
+		<img class="gallery__image" src="${galleryItems.preview}" alt="${galleryItems.description}"/>
+	</a>`, '');
 
 galleryConteiner.innerHTML = galleryImages;
 
-new SimpleLightbox('.gallery a', {
-	captionsData: 'alt',
+new SimpleLightbox(".gallery a", {
 	captionDelay: 250,
-	captionPosition: 'bottom',
-	captionType: 'attr',
 });
-
-/* galleryConteiner.addEventListener('click', (evt) => {
-	evt.preventDefault();
-
-	if (evt.target.nodeName !== 'IMG' || evt.target === evt.currentTarget) {
-		return;
-	};
-}); */
